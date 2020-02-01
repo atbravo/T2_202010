267d38cd396e031190db9580d87768ffbe049b5d
atbravo <at.bravo@uniandes.edu.co>
2020-01-31 20:53:52 -0500

    lista

---
+++ src/model/data_structures/ILista.java
@@ -0,0 +1,38 @@
+package model.data_structures;
+
+public interface ILista<T>
+{
+	public void agregarElemento(T elem);
+	
+	public void eliminarElemento(T elem);
+	/**
+	 * Retorna la cantidad de elementos en la lista
+	 * @return cantidad de elementos en la lista
+	 */
+	public int darTamaño();
+	/**
+	 * Retorna el elemento en la posicion dada por parametro inciando desde la posicion 0
+	 * @param pos 
+	 * @return elemento en la posicion dada si no lo encuentra retorna null
+	 */
+	public T darElementoPosicion(int pos);
+	/**
+	 * Avanza i veces en la lista respecto al elemento acutal y almacena este elemento como el actual,
+	 * si no existe dicho elemento almacena la cabeza como actual
+	 * @param i
+	 * @return el elemento en la nueva posicion actual si existe dicha posicion o la cabeza de lista de lo contrario
+	 */
+	public T recorrerActual(int i);
+	/**
+	 * Retorna el elemento guardado como actual.
+	 * @return elemento actual, por defecto es la cabeza de lista o null si no hay elemetos en la lista
+	 */
+	public T darElementoActual();
+	/**
+	 * Se almacena al elemento siguiente del actual como el nuevo elemento actual
+	 * @return el nuevo elemento actual, la cabeza delista	/**
+	 * Almacena al elemeto anterior al actual como el nuevo elemento actual
+	 * @return el nuevo elemento actual, la cabeza de lista si el elemento actual era el primero antes de llamar el metodo
+	 */
+	public T volverActual();
+}
--- src/model/data_structures/Lista.java
+++ src/model/data_structures/Lista.java
@@ -1,48 +1,134 @@
 package model.data_structures;
 
-public class Lista<T>
-{
+public class Lista<T> implements ILista<T> {
 	private Node cabeza;
 	private Node actual;
-	
-	public Lista()
-	{
+
+	public Lista() {
 		cabeza = null;
 		actual = null;
 	}
-	
-	public void asignarCabeza(T cabe)
-	{
+
+	/**
+	 * Asigna el nodo dado por parametro como el primero de la lista y el actual
+	 * 
+	 * @param cabe
+	 */
+	public void crearLista(T cabe) {
 		cabeza = new Node<T>(cabe);
 		actual = cabeza;
 	}
-	public void agregarElemento(T elemento)
-	{
+
+	/**
+	 * Agrega el elemento dado por parametro al final de la lista
+	 */
+	public void agregarElemento(T elemento) {
 		Node nuevo = new Node<T>(elemento);
-		if(cabeza == null)
-			asignarCabeza(elemento);
-		else
-		{
+		if (cabeza == null)
+			crearLista(elemento);
+		else {
 			Node actual = cabeza;
 			Node siguiente = cabeza.darSiguiente();
-			while(siguiente !=null)
-			{
+			while (siguiente != null) {
 				actual = siguiente;
 				siguiente = actual.darSiguiente();
 			}
 			actual.asignarSiguiente(nuevo);
 		}
 	}
-	public void eliminarElemento(T elemento)
-	{
+
+	/**
+	 * Elimina al elemento dado por parametro, si este no existe no hace nada
+	 */
+	public void eliminarElemento(T elemento) {
 		Node act = cabeza;
-		
-		while(act != null)
-		{
-			if(cabeza != null && cabeza.darElemento().equals(elemento))
-				cabeza = cabeza.darSiguiente();
-			else if(actual.darSiguiente().darElemento().equals(elemento))
+
+		if (cabeza != null && cabeza.darElemento().equals(elemento)) {
+			cabeza = cabeza.darSiguiente();
+			return;
+		}
+
+		while (act != null && act.darSiguiente() != null) {
+			if (actual.darSiguiente().darElemento().equals(elemento))
 				actual.borrarSiguiente();
 		}
 	}
+
+	@Override
+	public int darTamaño() {
+		Node act = cabeza;
+		int i = 0;
+		while (act != null) {
+			i++;
+			act = act.darSiguiente();
+		}
+		return i;
+	}
+
+	@Override
+	public T darElementoPosicion(int pos) {
+		Node act = cabeza;
+		int i = 0;
+		while (act != null && i < pos) {
+			i++;
+			act = act.darSiguiente();
+		}
+		if (act == null)
+			return null;
+		return (T) act.darElemento();
+	}
+
+	@Override
+	public T recorrerActual(int i) {
+		Node act = cabeza;
+		while (act != null && i > 0) {
+			act = act.darSiguiente();
+			i--;
+		}
+		if (act == null && cabeza == null) {
+			actual = null;
+			return null;
+		} else if (act == null)
+			actual = cabeza;
+		return (T) actual.darElemento();
+	}
+
+	@Override
+	public T darElementoActual() {
+		if (actual != null)
+			return (T) actual.darElemento();
+		return null;
+	}
+
+	public T avanzarActual() {
+		if (actual != null)
+			actual = actual.darSiguiente();
+		if (actual == null && cabeza == null)
+			return null;
+		else if (actual == null)
+			actual = cabeza;
+		return (T) actual.darElemento();
+
+	}
+
+	@Override
+	public T volverActual() {
+		Node act = cabeza;
+		boolean ya = false;
+
+		if (cabeza == null)
+			return null;
+		else if (!actual.equals(cabeza)) {
+			while (act != null && act.darSiguiente() != null && !ya) {
+				if (act.darSiguiente().equals(actual)) {
+					actual = act;
+					ya = true;
+				}
+
+				actual = actual.darSiguiente();
+			}
+		}
+		return (T) actual.darElemento();
+	}
+
 }
